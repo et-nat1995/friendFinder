20f9ab890e5ea615202d8a9320360735e840fb43
et-nat1995 <tamirnatanov@Tamirs-MacBook-Pro-2.local>
2018-11-08 22:15:10 -0700

    pretty mush done

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,21 @@
 var friends = [
-    
+    {
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4
+        ]
+    },
+    {
+        "name": "John",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            3, 2, 6, 4
+        ]
+    }
 ];
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -15,6 +15,26 @@ module.exports = app => {
             req.body.scores[i] = parseInt(req.body.scores[i]);
         }
 
+        var totDiff;
+        var diffArry = [];
+        var newFriend = req.body;
+
+        for (var i = 0; i < friends.length; i++) {
+            totDiff = 0;
+            for (var j = 0; j < newFriend.scores.length; j++) {
+                totDiff += Math.abs(friends[i].scores[j] - newFriend.scores[j]);
+            } //for j
+            diffArry.push(totDiff);
+        }
+
+        var match = diffArry.indexOf(Math.min(...diffArry));
+        console.log(match);
+
+        console.log(diffArry);
+
+
+        res.send(friends[match]);
         friends.push(req.body);
+
     });
 }
